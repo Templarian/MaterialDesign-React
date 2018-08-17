dff94c3d81709dbc10138113ce9e1c655f747ab4
Anton Zdanov <azdanov@protonmail.com>
2018-08-17 06:41:45 +0300

    refactor(Icon): convert to stateless component

--- src/Icon.js
+++ src/Icon.js
@@ -1,58 +1,50 @@
-import React, { Component } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import React from "react";
 
-class Icon extends Component {
-  render() {
-    const {
-      path, size, horizontal, vertical,
-      rotate, color, spin
-    } = this.props;
-    const width = isNaN(size)
-      ? size
-      : `${size * 1.5}rem`;
-    const transform = [];
-    if (horizontal) {
-      transform.push('scaleX(-1)');
-    }
-    if (vertical) {
-      transform.push('scaleY(-1)');
-    }
-    if (rotate !== 0) {
-      transform.push(`rotate(${rotate}deg)`);
-    }
-    const spinSec = spin === true || isNaN(spin) ? 2 : spin;
-    return (
-      <svg viewBox="0 0 24 24"
-        style={{
-          width,
-          transform: transform.join(' '),
-          animation: spin && `spin linear ${spinSec}s infinite`,
-          transformOrigin: spin && 'center'
-        }}>
-        {spin && <style>{'@keyframes spin { to { transform: rotate(360deg) } }'}</style>}
-        <path d={path}
-          style={{
-            fill: color
-          }}/>
-      </svg>
-    );
+const Icon = ({ path, size, horizontal, vertical, rotate, color, spin }) => {
+  const width = Number.isNaN(size) ? size : `${size * 1.5}rem`;
+  const transform = [];
+  if (horizontal) {
+    transform.push("scaleX(-1)");
   }
-}
+  if (vertical) {
+    transform.push("scaleY(-1)");
+  }
+  if (rotate !== 0) {
+    transform.push(`rotate(${rotate}deg)`);
+  }
+  const spinSec = spin === true || Number.isNaN(spin) ? 2 : spin;
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      style={{
+        width,
+        transform: transform.join(" "),
+        animation: spin && `spin linear ${spinSec}s infinite`,
+        transformOrigin: spin && "center"
+      }}
+    >
+      {spin && (
+        <style>{"@keyframes spin { to { transform: rotate(360deg) } }"}</style>
+      )}
+      <path
+        d={path}
+        style={{
+          fill: color
+        }}
+      />
+    </svg>
+  );
+};
 
 Icon.propTypes = {
   path: PropTypes.string.isRequired,
-  size: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.string,
-  ]),
+  size: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   horizontal: PropTypes.bool,
   vertical: PropTypes.bool,
   rotate: PropTypes.number,
   color: PropTypes.string,
-  spin: PropTypes.oneOfType([
-    PropTypes.bool,
-    PropTypes.number
-  ])
+  spin: PropTypes.oneOfType([PropTypes.bool, PropTypes.number])
 };
 
 Icon.defaultProps = {
